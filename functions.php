5339fa631a700763f760d2e8e75843c135423d4f
Wojciech Sobczak <wsobczak@gmail.com>
2021-07-12 12:17:34 +0200

    Init repo and add first, important files

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * AkrisMed functions and definitions
+ *
+ * @link https://bitbucket.org/vbert/akrismed/src/master/
+ *
+ * @package WordPress
+ * @subpackage AkrisMed
+ * @since AkrisMed 1.0
+ */
