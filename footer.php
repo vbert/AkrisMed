316410287c23359c07b9b4c1cb24a8fc259016d6
Wojciech Sobczak <wsobczak@gmail.com>
2021-07-13 14:25:58 +0200

    Add files header.php and footer.php

--- footer.php
+++ footer.php
@@ -1,16 +1,14 @@
 <?php
 /**
+ * The template for displaying the footer.
+ * 
  * @link https://bitbucket.org/vbert/akrismed/src/master/
  *
  * @package WordPress
  * @subpackage AkrisMed
  * @since AkrisMed 1.0
  */
-
-get_header();
 ?>
 
-
-
-<?php
-get_footer();
+</body>
+</html>
